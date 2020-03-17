31b851b53dc5a49f098f5949e41a63bdd58698f1
goepi <goepi@gmail.com>
2020-03-18 00:35:18 +0100

    update dependencies

--- packages/component-library/rollup.config.js
+++ packages/component-library/rollup.config.js
@@ -1,14 +1,11 @@
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-    input: 'src/index.tsx',
-    output: {
-        dir: './build',
-        format: 'cjs'
-    },
-    plugins: [
-        commonjs(),
-        typescript({ tsconfig: '../../tsconfig.json'}),
-    ]
+  input: 'src/index.tsx',
+  output: {
+    dir: './build',
+    format: 'cjs',
+  },
+  plugins: [commonjs(), typescript({ tsconfig: '../../tsconfig.json', verbosity: 3 })],
 };
