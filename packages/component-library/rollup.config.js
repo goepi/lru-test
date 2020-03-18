cc4f839297b28cbe2465616feb8c417f440307c2
goepi <goepi@gmail.com>
2020-03-18 01:29:53 +0100

    setup babel

--- packages/component-library/rollup.config.js
+++ packages/component-library/rollup.config.js
@@ -1,11 +1,17 @@
-import typescript from 'rollup-plugin-typescript2';
-import commonjs from '@rollup/plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+const extensions = ['.ts', '.js', '.tsx'];
 
+const globals = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+};
 export default {
   input: 'src/index.tsx',
   output: {
     dir: './build',
     format: 'cjs',
   },
-  plugins: [commonjs(), typescript({ tsconfig: '../../tsconfig.json', verbosity: 3 })],
+  plugins: [resolve({ extensions }), babel({ include: '**/packages/**/*', extensions })],
+  external: Object.keys(globals),
 };
